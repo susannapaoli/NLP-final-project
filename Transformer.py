119feda9d024fe9eb0aac5c8079c64c2d1231de2
Susanna Paoli <102821474+susannapaoli@users.noreply.github.com>
2023-04-16 23:06:46 +0200

    Update Transformer.py

--- Transformer.py
+++ Transformer.py
@@ -152,9 +152,9 @@ class TransformerTranslator(nn.Module):
         embeddings_enc = self.embed(inputs)
         
        for i in range(self.n_layers):
-            attention_enc = self.multi_head_attention_mask(embeddings_enc)
-            ff_ff = self.feedforward_layer(attention_enc)
-            embeddings_enc = ff_ff
+        attention_enc = self.multi_head_attention_mask(embeddings_enc)
+        ff_ff = self.feedforward_layer(attention_enc)
+        embeddings_enc = ff_ff
         
         
         outputs = self.final_layer(ff_ff)
