4ac9a4c32c0153726ec55e709953f8c0967d4930
Susanna Paoli <102821474+susannapaoli@users.noreply.github.com>
2023-04-16 23:05:13 +0200

    Update Transformer.py

--- Transformer.py
+++ Transformer.py
@@ -150,20 +150,12 @@ class TransformerTranslator(nn.Module):
         #############################################################################
     
         embeddings_enc = self.embed(inputs)
-        embeddings_dec = self.embed(target)
         
-        attention_enc = self.multi_head_attention(embeddings_enc)
-        ff = self.feedforward_layer(attention_enc)
+       for i in range(self.n_layers):
+            attention_enc = self.multi_head_attention_mask(embeddings_enc)
+            ff_ff = self.feedforward_layer(attention_enc)
+            embeddings_enc = ff_ff
         
-       
-            
-        attention_decoder = self.multi_head_attention_mask(embeddings_dec)
-        concat = torch.cat((attention_decoder, ff))
-        
-        for i in range(self.n_layers):
-            attention_dec = self.multi_head_attention(concat)
-            ff_ff = self.feedforward_layer(attention_dec)
-            concat = ff_ff
         
         outputs = self.final_layer(ff_ff)
     
