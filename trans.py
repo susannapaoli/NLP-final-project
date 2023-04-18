4d471d8bb8390f83097779aa998a6eb479bf2a3c
Susanna Paoli <102821474+susannapaoli@users.noreply.github.com>
2023-04-18 22:20:01 +0200

    create mask

--- trans.py
+++ trans.py
@@ -22,12 +22,35 @@ class Transformer(nn.Module):
         self.dropout = nn.Dropout(dropout)
         self.device = device
 
-    def forward(self, src, tgt, src_mask, tgt_mask, src_padding_mask, tgt_padding_mask):
-        src_len, N = src.shape
-        tgt_len, N = tgt.shape
+    def forward(self, src, tgt):
+        # src shape: (src_seq_len, N)
+        # tgt shape: (tgt_seq_len, N)
+        src_padding_mask = self.create_padding_mask(src)
+        tgt_padding_mask = self.create_padding_mask(tgt)
+        src_mask = self.create_src_mask(src)
+        tgt_mask = self.create_tgt_mask(tgt)
         src = self.dropout(self.src_embedding(src))
         tgt = self.dropout(self.tgt_embedding(tgt))
         enc_src = self.encoder(src, src_mask, src_padding_mask)
         output, _ = self.decoder(tgt, enc_src, tgt_mask, None, tgt_padding_mask, None)
         output = self.fc_out(output)
         return output
+
+    def create_padding_mask(self, seq):
+        # seq shape: (seq_len, N)
+        mask = (seq == self.src_embedding.padding_idx).transpose(0, 1)
+        return mask
+
+    def create_src_mask(self, src):
+        # src shape: (src_seq_len, N)
+        src_seq_len = src.shape[0]
+        mask = torch.triu(torch.ones(src_seq_len, src_seq_len, device=self.device), diagonal=1).bool()
+        return mask
+
+    def create_tgt_mask(self, tgt):
+        # tgt shape: (tgt_seq_len, N)
+        tgt_seq_len = tgt.shape[0]
+        mask = torch.triu(torch.ones(tgt_seq_len, tgt_seq_len, device=self.device), diagonal=1).bool()
+        tgt_padding_mask = self.create_padding_mask(tgt)
+        mask = mask.masked_fill(tgt_padding_mask.unsqueeze(1).unsqueeze(2), True)
+        return mask
