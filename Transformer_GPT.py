2986ac32a19f2172610fc2ecf05ed8d19a1fbdf5
PierluigiManc01 <106546252+PierluigiManc01@users.noreply.github.com>
2023-04-20 16:50:34 -0400

    Update Transformer_GPT.py

--- Transformer_GPT.py
+++ Transformer_GPT.py
@@ -1,22 +1,4 @@
-"""
-Transformer model.  (c) 2021 Georgia Tech
-Copyright 2021, Georgia Institute of Technology (Georgia Tech)
-Atlanta, Georgia 30332
-All Rights Reserved
-Template code for CS 7643 Deep Learning
-Georgia Tech asserts copyright ownership of this template and all derivative
-works, including solutions to the projects assigned in this course. Students
-and other users of this template code are advised not to share it with others
-or to make it available on publicly viewable websites including repositories
-such as Github, Bitbucket, and Gitlab.  This copyright statement should
-not be removed or edited.
-Sharing solutions with current or future students of CS 7643 Deep Learning is
-prohibited and subject to being investigated as a GT honor code violation.
------do not edit anything above this line---
-"""
-
 import numpy as np
-
 import torch
 from torch import nn
 import random
